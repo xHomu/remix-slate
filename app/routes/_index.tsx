6aa31a7defbd4bc03954cbf7e8d380a4c871ed20
xHomu <84349818+xHomu@users.noreply.github.com>
2024-01-05 17:03:17 -0800

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -46,46 +46,4 @@ export default function Index() {
     </Slate>
   );
 }
-
-// const initialValue = [
-//   {
-//     type: 'paragraph',
-//     children: [{ text: 'A line of text in a paragraph.' }],
-//   },
-// ]
-
-// const App = () => {
-//   const [editor] = useState(() => withReact(createEditor()))
-
-//   const renderElement = useCallback(props => {
-//     switch (props.element.type) {
-//       case 'code':
-//         return <CodeElement {...props} />
-//       default:
-//         return <DefaultElement {...props} />
-//     }
-//   }, [])
-
-//   return (
-//     <Slate editor={editor} initialValue={initialValue}>
-//       <Editable
-//         renderElement={renderElement}
-//         onKeyDown={event => {
-//           if (event.key === '`' && event.ctrlKey) {
-//             event.preventDefault()
 //             // Determine whether any of the currently selected blocks are code blocks.
-//             const [match] = Editor.nodes(editor, {
-//               match: n => n.type === 'code',
-//             })
-//             // Toggle the block type depending on whether there's already a match.
-//             Transforms.setNodes(
-//               editor,
-//               { type: match ? 'paragraph' : 'code' },
-//               { match: n => Element.isElement(n) && Editor.isBlock(editor, n) }
-//             )
-//           }
-//         }}
-//       />
-//     </Slate>
-//   )
-// }
